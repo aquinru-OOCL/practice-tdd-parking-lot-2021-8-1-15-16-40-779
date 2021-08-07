b784450ef4b8d3a6e9a9ff389f2aa585799c2b8f
AQUINRU <russel.aquino@oocl.com>
2021-08-07 19:01:32 +0800

    [Story5][Russel] feat: should_park_in_parking_lot_with_most_available_positions__when_park_given_a_smart_parking_boy_who_manage_two_parking_lots_both_with_available_position

--- src/main/java/com/parkinglot/Process/SmartParking.java
+++ src/main/java/com/parkinglot/Process/SmartParking.java
@@ -5,14 +5,14 @@ import com.parkinglot.Exceptions.NoAvailablePositionException;
 import com.parkinglot.ParkingLot;
 import com.parkinglot.ParkingTicket;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class SmartParking implements ParkingStyle{
     @Override
     public ParkingTicket park(Car car, List<ParkingLot> parkingLots) {
         return parkingLots.stream()
-                .filter(parkingLot -> !parkingLot.isParkingLotFull())
-                .findFirst()
+                .max(Comparator.comparingInt(ParkingLot::getAvailableCapacity))
                 .orElseThrow(NoAvailablePositionException::new)
                 .park(car);
     }
